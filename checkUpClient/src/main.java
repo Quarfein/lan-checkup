789e2835c7dd90f1da4212024a1886b4491efcad
Quarfein <41216613+Quarfein@users.noreply.github.com>
2022-03-09 11:55:10 +0100

    Adding main client program

---
+++ checkUpClient/src/main.java
@@ -0,0 +1,9 @@
+import lanClient.*;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		client client = new client("134.59.139.211",5000);
+	}
+
+}
