fe480ec242a1dfd8eb9309eb15a87acea00b1963
just maxime <jm002743@rtbase2lin.campus.unice.fr>
2022-03-25 16:48:59 +0100

    threads work

--- checkUpClient/src/main.java
+++ checkUpClient/src/main.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import lanClient.*;
 public class main {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		client client = new client("127.0.0.1",5000);
+		List<String> addr = new ArrayList<String>();
+		
+		addr.add("134.59.139.50");
+		addr.add("127.0.0.1");
+		
+		addr.forEach((address)->{
+			Thread th = new Thread(){
+				public void run() {
+					try {
+						client client = new client(address, 5000);
+					} catch (ClassNotFoundException e) {
+						
+						e.printStackTrace();
+					}
+				}
+			};
+			th.start();
+		});		
+		//client client = new client("127.0.0.1",5000);
 	}
 
 }
