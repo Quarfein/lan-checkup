8a8814f77c90d808735c5f38a353f1888ee85ec2
just maxime <jm002743@rtbase2lin.campus.unice.fr>
2022-03-25 14:59:57 +0100

    client ready but test class not launching?

--- checkUpClient/src/lanClient/client.java
+++ checkUpClient/src/lanClient/client.java
@@ -1,29 +1,37 @@
 package lanClient;
 
 import java.net.*;
+import java.util.List;
 import java.io.*;
 
 public class client {
 	// initialize socket and input output streams
     private Socket socket            = null;
-    private DataInputStream  in   = null;
-    private DataOutputStream out     = null;
+    int closeConnection = 0;
  
     // constructor to put ip address and port
-    public client(String address, int port)
+    public client(String address, int port) throws ClassNotFoundException
     {
         // establish a connection
         try
         {
             socket = new Socket(address, port);
-            System.out.println("Connected");
  
-            // takes input from terminal
-            in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
- 
-            // sends output to the socket
-            out    = new DataOutputStream(socket.getOutputStream());
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            try
+            {
+            	while(closeConnection!=1) {
+            	@SuppressWarnings("unchecked")
+				List<String> received = (List<String>) objectInputStream.readObject();
+            	System.out.println(received);
+            	}
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
+                closeConnection = 1;
+            }
         }
         catch(UnknownHostException u)
         {
@@ -34,29 +42,9 @@ public class client {
             System.out.println(i);
         }
  
-        // string to read message from input
- 
-        // keep reading until "Over" is input
-            try
-            {
-            	String line;
-                out.writeUTF("test");
-                line = in.readUTF();
-                if (line == "succeed") {
-                	System.out.println(line);
-                	out.writeUTF("Over");
-                }
-            }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
- 
         // close the connection
         try
         {
-            in.close();
-            out.close();
             socket.close();
         }
         catch(IOException i)
--- checkUpClient/src/main.java
+++ checkUpClient/src/main.java
@@ -1,7 +1,7 @@
 import lanClient.*;
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
 		client client = new client("127.0.0.1",5000);
 	}
